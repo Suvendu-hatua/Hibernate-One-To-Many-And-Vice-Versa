94da3d52a86fc0e20ff30712ed0c0eae81d9cd68
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-11 12:06:55 +0530

    Implementing createInstructor, findInstructorByID and delete methods in dao files.

--- src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/dao/AppDao.java
+++ src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/dao/AppDao.java
@@ -1,4 +1,13 @@
 package com.spring_boot.hibernate.Hibernate_One_To_Many.dao;
 
+import com.spring_boot.hibernate.Hibernate_One_To_Many.entity.Course;
+import com.spring_boot.hibernate.Hibernate_One_To_Many.entity.Instructor;
+
 public interface AppDao {
+
+    public void addInstructorWithDetailsAndCourse(Instructor instructor);
+    public Instructor findInstructorById(int id);
+    public Course findCourseById(int id);
+    public void deleteCourseById(int id);
+    public void deleteInstructorById(int id);
 }
--- src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/dao/AppDaoImpl.java
+++ src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/dao/AppDaoImpl.java
@@ -1,4 +1,50 @@
 package com.spring_boot.hibernate.Hibernate_One_To_Many.dao;
 
+import com.spring_boot.hibernate.Hibernate_One_To_Many.entity.Course;
+import com.spring_boot.hibernate.Hibernate_One_To_Many.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
 public class AppDaoImpl implements AppDao{
+    private EntityManager entityManager;
+
+    @Autowired
+    public  AppDaoImpl(EntityManager entityManager){
+        this.entityManager=entityManager;
+    }
+    @Override
+    @Transactional
+    public void addInstructorWithDetailsAndCourse(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int id) {
+
+        return entityManager.find(Instructor.class,id);
+    }
+
+    @Override
+    public Course findCourseById(int id) {
+        return entityManager.find(Course.class,id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteCourseById(int id) {
+        Course course=findCourseById(id);
+        //removing relationship with instructor.
+        course.getInstructor().setCourses(null);
+        entityManager.remove(course);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+        Instructor instructor=findInstructorById(id);
+        entityManager.remove(instructor);
+    }
 }
