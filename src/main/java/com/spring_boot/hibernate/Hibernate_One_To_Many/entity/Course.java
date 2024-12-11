9d58bdc9b624db1062fdc8cfd9ce13a3aa7e2dfb
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-11 11:41:16 +0530

    Updating Course pojo and also making ManyToOne relationship with Instructor

---
+++ src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/entity/Course.java
@@ -0,0 +1,66 @@
+package com.spring_boot.hibernate.Hibernate_One_To_Many.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "course")
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title",nullable = false)
+    private String title;
+
+    @Column(name = "description",nullable = false)
+    private String description;
+
+    @ManyToOne(cascade = {CascadeType.MERGE,CascadeType.DETACH,CascadeType.PERSIST,CascadeType.REFRESH})
+    @JoinColumn(name = "instructor_id")
+    private Instructor instructor;
+    //Adding constructor
+
+    public Course() {
+    }
+
+    public Course(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+    //Adding Setter and getter methods
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Instructor getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(Instructor instructor) {
+        this.instructor = instructor;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
--- src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/entity/Couse.java
+++
@@ -1,4 +0,0 @@
-package com.spring_boot.hibernate.Hibernate_One_To_Many.entity;
-
-public class Couse {
-}
