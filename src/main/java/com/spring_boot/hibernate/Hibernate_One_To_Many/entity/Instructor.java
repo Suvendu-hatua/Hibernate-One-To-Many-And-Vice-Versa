4a0f725993cc31131ec485945e2b70e1b6869d2b
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-11 11:41:52 +0530

    Updating Instructor pojo and also making OneToOne and OneToMany relationship with InstructorDetails and Course

--- src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/entity/Instructor.java
+++ src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/entity/Instructor.java
@@ -1,4 +1,92 @@
 package com.spring_boot.hibernate.Hibernate_One_To_Many.entity;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "instructor")
 public class Instructor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first-name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last-name")
+    private String lastName;
+
+    @Column(name = "email",nullable = false)
+    private String email;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "instructor-details-id",referencedColumnName = "id")
+    private InstructorDetails instructorDetails;
+
+    @OneToMany(mappedBy = "instructor",cascade = {CascadeType.DETACH,CascadeType.MERGE,CascadeType.PERSIST,CascadeType.REFRESH})
+    private List<Course> courses;
+
+    //Adding Constructor
+
+    public Instructor() {
+    }
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    //Adding setter and getter methods.
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public InstructorDetails getInstructorDetails() {
+        return instructorDetails;
+    }
+
+    public void setInstructorDetails(InstructorDetails instructorDetails) {
+        this.instructorDetails = instructorDetails;
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
