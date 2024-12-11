bc664bf1393bb791c69403e1374b70a1138dc57c
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-11 10:51:17 +0530

    git init

---
+++ src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/HibernateOneToManyApplication.java
@@ -0,0 +1,13 @@
+package com.spring_boot.hibernate.Hibernate_One_To_Many;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateOneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateOneToManyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot/hibernate/Hibernate_One_To_Many/HibernateOneToManyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot.hibernate.Hibernate_One_To_Many;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateOneToManyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
