c1f0bed8bfa893ed98473c90faf55e2ca29a7bef
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-15 12:45:40 +0530

    Adding Review Pojo for adding reviews associated course.

---
+++ src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/entity/Review.java
@@ -0,0 +1,61 @@
+package com.spring_boot.hibernate.Hibernate_One_To_Many.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "review")
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+
+    //Constructor
+
+    public Review() {
+    }
+
+    //Setter and Getter method()
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    //toString() method
+
+    @Override
+    public String toString() {
+        return "Review{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
