4a0b1cda294ec5abbff49ec62a83c75323cf0e81
Suvendu Hatua <suvenduhatua01@gmail.com>
2024-12-15 13:11:28 +0530

    Adding One-To-Many relationship with Review

--- src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/entity/Course.java
+++ src/main/java/com/spring_boot/hibernate/Hibernate_One_To_Many/entity/Course.java
@@ -2,6 +2,9 @@ package com.spring_boot.hibernate.Hibernate_One_To_Many.entity;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "course")
 public class Course {
@@ -19,8 +22,12 @@ public class Course {
     @ManyToOne(cascade = {CascadeType.MERGE,CascadeType.DETACH,CascadeType.PERSIST,CascadeType.REFRESH})
     @JoinColumn(name = "instructor_id")
     private Instructor instructor;
-    //Adding constructor
 
+    //Adding One-To-Many relationship to Review
+    @OneToMany(mappedBy = "course", cascade = CascadeType.ALL)
+    private List<Review> reviews;
+
+    //Adding constructor
     public Course() {
     }
 
@@ -53,6 +60,21 @@ public class Course {
 
     public void setInstructor(Instructor instructor) {
         this.instructor = instructor;
+    }
+
+    public List<Review> getReviews() {
+        return reviews;
+    }
+
+    public void setReviews(List<Review> reviews) {
+        this.reviews = reviews;
+    }
+
+    public void addReview(Review review){
+        if(reviews==null){
+            reviews=new ArrayList<>();
+        }
+        reviews.add(review);
     }
 
     @Override
